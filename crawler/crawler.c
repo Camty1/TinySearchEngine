840b4ca5d5008156ca44fc364331f1e16d0fe9e7
Hannah Brookes <hannah.m.brookes.23@dartmouth.edu>
2023-02-02 11:22:36 -0500

    made crawler.c file

---
+++ crawler/crawler.c
@@ -0,0 +1,19 @@
+/* crawler.c --- 
+ * 
+ * 
+ * Author: Hannah M. Brookes
+ * Created: Thu Feb  2 11:06:04 2023 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+#include <../utils/queue.h>
+#include <../utils/hash.h>
+#include <../utils/webpage.h>
+
+int main() {
+	printf("Hello\n");
+}
