4fe9781e4fa03716eeefc9540da16a726a9300c8
Hannah Brookes <hannah.m.brookes.23@dartmouth.edu>
2023-02-02 17:50:44 -0500

    draft of step 3. prints out correct answer but generates valgrind errors

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -32,11 +32,12 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
+
+	// STEP 2: FETCH ONE PAGE
 	// scan fetched html and print all URLs and whether each URL is
 	// internal/external
 	int position = 0;
-	char *result;
-
+	char* result;
 	while ((position = webpage_getNextURL(web, position, &result)) > 0) {
 		char* int_ext;
 		if (IsInternalURL(result))
@@ -46,10 +47,37 @@ int main() {
 		printf("Found %s url: %s\n", int_ext, result);
 		free(result);
 	}
-
-	// deallocate webpage
+	
+ 
+	// STEP 3: QUEUE OF WEBPAGES
+	position = 0;
+	queue_t* queue_urls = qopen();
+	webpage_t* temp = NULL;
+	// iterate through all URLs of the given page
+	while ((position = webpage_getNextURL(web, position, &result)) > 0) {
+		// if the URL is internal, create a new webpage for the URL and
+		// place it in the queue
+		if (IsInternalURL(result)) {
+			temp = webpage_new(result, 1, NULL);
+			qput(queue_urls, temp);
+		}
+		free(result);
+	}
+	// prints the list of urls
+	webpage_t* temp_page = (webpage_t*)qget(queue_urls);
+	char* temp_url = NULL;
+	while (temp_page != NULL) {
+		temp_url = webpage_getURL(temp_page);
+		printf("%s in queue\n", temp_url);
+		temp_page = (webpage_t*)qget(queue_urls);	
+	}
+ 	// close the queue
+	qclose(queue_urls);
+	// deallocate webpages
  	webpage_delete(web);
-
+	webpage_delete(temp_page);
+	webpage_delete(temp);
+	
 	exit(EXIT_SUCCESS);
 
 }
