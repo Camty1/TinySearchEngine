d9bb4f38e0ae854d58c4b7870ed7329a946f510e
Cameron J. Wolfe <f003xv6@hub3.thayer.dartmouth.edu>
2023-02-03 13:21:52 -0500

    Passed Step 3, working on Step 4

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -16,6 +16,10 @@
 #include <hash.h>
 #include <webpage.h>
 
+#define HASH_SIZE 100
+
+bool compareURLs(void* page, const void* url);
+
 int main() {
 
 	// seed url
@@ -24,6 +28,8 @@ int main() {
 
 	// creates webpage using seed url
 	webpage_t* web = webpage_new(url, 0, NULL);
+
+    printf("%d\n", compareURLs(web, url));
 
 	// fetch webpage html and check that it passes
 	bool result_fetch = webpage_fetch(web);
@@ -47,37 +53,65 @@ int main() {
 		printf("Found %s url: %s\n", int_ext, result);
 		free(result);
 	}
-	
+    
+        
  
 	// STEP 3: QUEUE OF WEBPAGES
 	position = 0;
 	queue_t* queue_urls = qopen();
 	webpage_t* temp = NULL;
+
+    hashtable_t* visited = hopen(HASH_SIZE);
+    hput(visited, web, url, HASH_SIZE);
+    
+    char* testURL;
 	// iterate through all URLs of the given page
-	while ((position = webpage_getNextURL(web, position, &result)) > 0) {
+	while ((position = webpage_getNextURL(web, position, &testURL)) > 0) {
 		// if the URL is internal, create a new webpage for the URL and
 		// place it in the queue
-		if (IsInternalURL(result)) {
-			temp = webpage_new(result, 1, NULL);
-			qput(queue_urls, temp);
+
+		if (IsInternalURL(testURL)) {
+			temp = webpage_new(testURL, 1, NULL);
+            
+            // Check if page is in hash table, and if not, add it to the
+            // queue and the hash table
+            void* page = hsearch(visited, compareURLs, testURL, HASH_SIZE);
+            
+            if (page == NULL) {
+			    qput(queue_urls, temp);
+                hput(visited, temp, testURL, HASH_SIZE);
+            }
 		}
-		free(result);
 	}
+    
 	// prints the list of urls
 	webpage_t* temp_page = (webpage_t*)qget(queue_urls);
 	char* temp_url = NULL;
 	while (temp_page != NULL) {
 		temp_url = webpage_getURL(temp_page);
 		printf("%s in queue\n", temp_url);
+        webpage_delete(temp_page);
 		temp_page = (webpage_t*)qget(queue_urls);	
 	}
+
  	// close the queue
 	qclose(queue_urls);
+    hclose(visited);
 	// deallocate webpages
  	webpage_delete(web);
 	webpage_delete(temp_page);
-	webpage_delete(temp);
+	//webpage_delete(temp);
 	
 	exit(EXIT_SUCCESS);
 
+}
+
+bool compareURLs(void* page, const void* url) {
+    char* castedURL = (char*) url;
+    char* webpageURL = webpage_getURL((webpage_t*) page);
+    
+
+    bool result = strcmp(webpageURL, castedURL) == 0;
+    printf("%d:\n%s\n%s\n\n", result, castedURL, webpageURL);
+    return result;
 }
