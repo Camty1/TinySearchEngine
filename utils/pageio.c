cf31885d5e832f9334368cf21d6f6ffafc2be3ca
Camty1 <Cameron.J.Wolfe.23@dartmouth.edu>
2023-02-10 09:46:46 -0500

    Implemented, but not tested pageload function in pageio

--- utils/pageio.c
+++ utils/pageio.c
@@ -5,6 +5,8 @@
 #include <webpage.h>
 #include <pageio.h>
 
+#define MAX_URL_SIZE 100
+
 int32_t pagesave(webpage_t* pagep, int id, char* dirnm) {
 
     // Retrieve relevant information about given webpage
@@ -37,5 +39,57 @@ int32_t pagesave(webpage_t* pagep, int id, char* dirnm) {
 }
 
 webpage_t* pageload(int id, char* dirnm) {
-    return NULL;
+    
+    // Get filepath
+    char filepath[100];
+    sprintf(filepath, "%s/%d", dirnm, id);
+    
+    // Open file for reading
+    FILE* read_file; 
+    read_file = fopen(filepath, "r");
+
+    if (read_file == NULL) {
+        printf("Error opening file\n");
+        return NULL;
+    }
+    
+    // Initialize stack variables for webpage creation
+    char url[MAX_URL_SIZE];
+    int depth;
+    int html_len;
+   
+    // Get variables from file 
+    int scan_result = fscanf(read_file, "%s %d %d", url, &depth, &html_len); 
+    
+    if (scan_result != 3) {
+        printf("Error reading from file first three items\n");
+        return NULL;
+    }
+    
+    // Allocate memory for html
+    char* html = (char*) malloc(html_len * sizeof(char));
+    
+    // Create format string for fscanf
+    char formatString[30];
+    sprintf(formatString, "%c%d%c", '%', html_len-1, 'c');
+
+    // Read from file
+    scan_result = fscanf(read_file, formatString, html);
+
+    html[html_len-1] = '\0';
+
+    if (scan_result == 0) {
+        printf("Error reading html\n");
+        return NULL;
+    }
+
+    // Create webpage
+    webpage_t* page = webpage_new(url, depth, html);
+
+    if (page == NULL) {
+        printf("Error creating webpage\n");
+        return NULL;
+    }
+
+    return page;
 }
