981469d0656f8d5035523ff9c8db941c890b2c9c
Raif Olson <raif.t.olson.24@dartmouth.edu>
2023-02-21 10:58:44 -0500

    Create query.c

---
+++ querier/query.c
@@ -0,0 +1,29 @@
+/* query.c --- 
+ * 
+ * 
+ * Author: Raif T. Olson
+ * Created: Tue Feb 21 10:52:01 2023 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+
+#include <stdio.h>                                                            
+#include <stdlib.h>                                                           
+#include <stdint.h>                                                           
+#include <string.h>                                                           
+#include <ctype.h>                                                            
+#include <queue.h>                                                            
+#include <hash.h>                                                             
+#include <webpage.h>                                                          
+#include <pageio.h>                                                           
+#include <indexio.h>
+
+
+
+int main void(int argc, char* argv[]) {
+	
+	
+}
