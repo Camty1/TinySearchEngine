d317489633de8751178f100b3546e5aa8b1bb2cd
Camty1 <cameron.j.wolfe.23@dartmouth.edu>
2023-02-21 22:29:30 -0500

    Step 2 of module 6

--- querier/query.c
+++ querier/query.c
@@ -10,22 +10,36 @@
  */
 
 
-#include <stdio.h>                                                            
-#include <stdlib.h>                                                           
-#include <stdint.h>                                                           
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <stdbool.h>
-#include <string.h>                                                           
-#include <ctype.h>                                                            
-#include <queue.h>                                                            
-#include <hash.h>                                                             
-#include <webpage.h>                                                          
-#include <pageio.h>                                                           
+#include <string.h>
+#include <ctype.h>
+#include <queue.h>
+#include <hash.h>
+#include <webpage.h>
+#include <pageio.h>
 #include <indexio.h>
 
 #define MAX_QUERY 200
 
 static void printQueryWord(void* elementp);
 static int normalizeWord(char *word, int word_len);
+static int rankDocument(queue_t* queries, hashtable_t* index, int docId);
+static int getOccurence(hashtable_t* index, char* word, int docId);
+static bool compareId(void* elementp, const void* keyp);
+static bool compareWord(void* elementp, const void* keyp);
+
+typedef struct {
+    char* word;
+    queue_t* documentQueue;
+} wordQueue_t;
+
+typedef struct {
+    int documentId;
+    int count;
+} docWordCount_t;
 
 /* if (strcmp(query_word, "^D")) {
 	running = false;
@@ -35,29 +49,48 @@ static int normalizeWord(char *word, int word_len);
 */
 
 int main(int argc, char* argv[]) {
+    // Load in index
+    hashtable_t* index = indexLoad("step2");
+
 	bool running = true;
-	while (running) {
+	
+    // Query prompt loop
+    while (running) {
 		char search_query[MAX_QUERY];
 		char* query_word;
 		bool query_valid = true;
 		printf("> ");
+        // Get string from console
 		if (scanf("%[^\n]s", search_query) != EOF) {
-
+			
+            // Open query queue
+            queue_t* query_q = qopen();
+            
+            // Go through words in query
 			query_word = strtok(search_query, " ");
-			queue_t* query_q = qopen();
 			while (query_word != NULL) {
+
+                // Make sure word is valid
 				if (normalizeWord(query_word, strlen(query_word)) == 0) {
+                    // Put word in query queue
 					qput(query_q, query_word);
-				} else {
+				} 
+
+                else {
 					query_valid = false;
 					break;
 				}
 				query_word = strtok(NULL, " ");
 			}
+            
+            // Query is valid
 			if (query_valid) {
 				qapply(query_q, printQueryWord);
 				printf("\n");
-			} else {
+                int rank = rankDocument(query_q, index, 1); // Step 2
+			}
+
+            else {
 				printf("[invalid query]\n");
 			}
 			// flushes the standard input to allow for new query to be entered
@@ -75,23 +108,103 @@ int main(int argc, char* argv[]) {
 }
 
 
-static int normalizeWord(char *word, int word_len) {                          
-	for (int i=0; i<word_len; i++) {                                           
-		char letter = word[i];                                                   
-		int res = isalpha(letter);                                               
-		if (res != 0) {                                                          
-			if ((64 < letter) && (letter < 91)) {                                  
-				word[i] = tolower(letter);                                           
-			}                                                                     
-		} else {                                                                 
-			return -1;                                                             
-		}                                                                        
+static int normalizeWord(char *word, int word_len) {
+
+	for (int i=0; i<word_len; i++) {
+		char letter = word[i];
+		int res = isalpha(letter);
+	
+        if (res != 0) {
+		
+            if ((64 < letter) && (letter < 91)) {
+				word[i] = tolower(letter);
+			}
+		}
+        
+        else {
+			return -1;
+		}
 	}
-  
-	return 0;                                                                    
-}                                                                              
+    
+	return 0;
+}
 
 static void printQueryWord(void* elementp) {
 	char* query_word = (char*) elementp;
 	printf("%s ", query_word);
+}
+
+static int rankDocument(queue_t* queries, hashtable_t* index, int docId) {
+    // Variable initialization
+    char* word;
+    int rank = INT_MAX;
+
+    // Go through words in query queue
+    while ((word = (char*) qget(queries)) != NULL) {
+
+        // These words should be ignored
+        if (strlen(word) < 3 || strcmp(word, "and") == 0 || strcmp(word, "or") == 0) {
+            
+        }
+
+        else {
+            // Get number of occurences in index for a given document
+            int occurence = getOccurence(index, word, docId);
+            printf("%s:%d ", word, occurence);
+
+            // Tracking minimum occurence for rank
+            if (occurence < rank) {
+                rank = occurence;
+            }
+        }
+    }
+    // At least one word was valid and was given an occurence
+    if (rank != INT_MAX) {
+        printf("-- %d\n", rank);
+        return rank;
+    }
+    // Either query queue was empty or no valid words
+    else {
+        printf("-- 0\n");
+        return 0;
+    }
+}
+
+static int getOccurence(hashtable_t* index, char* word, int docId) {
+
+    // Variable initialization
+    int occurence = 0;
+
+    // Search for word in index
+    wordQueue_t* indexElement = (wordQueue_t*) hsearch(index, compareWord, word, strlen(word));
+
+    // Word is in index
+    if (indexElement != NULL) {
+
+        // Search for document in wordQueue
+        docWordCount_t* queueElement = (docWordCount_t*) qsearch(indexElement->documentQueue, compareId, (const void *) &docId);
+        
+        // Document is in queue
+        if (queueElement != NULL) {
+            occurence = queueElement->count;
+        }
+
+    }
+    
+    // Return number of occurences of word in given document
+    return occurence;
+}
+
+// Compare words to word queues for hsearch
+static bool compareWord(void* elementp, const void* keyp) {
+    wordQueue_t* wq = (wordQueue_t*) elementp;
+    char* key = (char*) keyp;
+    return (strcmp(wq->word, key) == 0);
+}
+
+// Compare ints to document word counts document ids
+static bool compareId(void* elementp, const void* keyp) {
+    docWordCount_t* wc = (docWordCount_t*) elementp;
+    int* id = (int*) keyp;
+    return (wc->documentId == *id);
 }
