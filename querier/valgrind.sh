7cd13a4d3992972c0ac375c801a6c8f196d6c917
Hannah Brookes <hannah.m.brookes.23@dartmouth.edu>
2023-02-27 15:02:32 -0500

    valgrind file

---
+++ querier/valgrind.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# == 5 ]]
+then
+		# valgrind for querier file
+		valgrind --leak-check=full --show-leak-kinds=all ./query.bin $1 $2 $3 $3 $5 2> valgrind_err > valgrind_out
+
+		if [[ $(grep 'no leaks are possible' valgrind_err | wc -l) -eq 1 && $(grep '0 errors' valgrind_err | wc -l) -eq 1 ]]
+		then
+				echo "query.bin passed valgrind check"
+		else
+				echo "query.bin FAILED valgrind check"
+				cat valgrind_err
+		fi
+		rm -r valgrind_err
+		rm -r valgrind_out
+else
+		echo "usage error: valgrind.sh <pagedir> <indexnm> -q <myqueries.txt> <outputFile>"
+fi
