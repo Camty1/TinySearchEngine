6076ea167abd5de5d9ddeeb8a8b29e6ef060dd17
Hannah Brookes <hannah.m.brookes.23@dartmouth.edu>
2023-02-28 19:17:55 -0500

    create interface for locked-queue module

---
+++ utils/lqueue.c
@@ -0,0 +1,19 @@
+/* lqueue.c --- implementation of lqueue.h interface
+ * 
+ * 
+ * Author: Hannah M. Brookes
+ * Created: Tue Feb 28 19:01:26 2023 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <queue.h>
+#include <lqueue.h>
+#include <pthread.h>
+
---
+++ utils/lqueue.h
@@ -0,0 +1,30 @@
+#pragma once
+/* lqueue.h --- 
+ * 
+ * 
+ * Author: Hannah M. Brookes
+ * Created: Tue Feb 28 19:03:13 2023 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include <queue.h>
+
+typedef void lqueue_t;
+
+lqueue_t* lqopen(void);
+
+void lqclose(lqueue_t *lqp);
+
+int32_t lqput(lqueue_t *lqp, void *elementp);
+
+void* lqget(lqueue_t *lqp);
+
+void lqapply(lqueue_t *lqp, void (*fn)(void* elementp));
+
+void lqsearch(lqueue_t *lqp,
+							bool (*searchfn)(void *elementp, const void *keyp),
+							const void *skeyp);
